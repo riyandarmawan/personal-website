221d073b13b2af7dbc92e598ad960d256edfca0f
Riyan042 <riyandarmawan042@gmail.com>
2024-04-06 07:31:51 +0700

    home finished

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,17 @@
 // autotype effect
 const profile = document.querySelector("#profile");
-const profileTexts = ["Web Developer", "Software Engineer", "Full-Stack Developer", "Language Learner"];
+const profileTexts = [
+    "Web Developer",
+    "Software Engineer",
+    "Full-Stack Developer",
+    "Language Learner",
+];
 
 function autoType(element, text, i = 1) {
     setInterval(() => {
         element.textContent = text[i];
         i++;
-        if(i === text.length) {
+        if (i === text.length) {
             i = 0;
         }
     }, 4000);
