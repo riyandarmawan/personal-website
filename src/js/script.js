983f604c0716ebbb8ca4efe8cfb1a964dbca18b5
Riyan042 <riyandarmawan042@gmail.com>
2024-04-08 08:33:05 +0700

    home fixed

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,37 @@
-// autotype effect
+// autotype profile
 const profile = document.querySelector("#profile");
 const profileTexts = [
-    "Web Developer",
-    "Software Engineer",
-    "Full-Stack Developer",
-    "Language Learner",
+  "Web Developer",
+  "Full-Stack Developer",
+  "Software Engineer Student",
+  "Language Learner",
 ];
 
-function autoType(element, text, i = 1) {
-    setInterval(() => {
-        element.textContent = text[i];
-        i++;
-        if (i === text.length) {
-            i = 0;
-        }
-    }, 4000);
+function autoType(
+  element,
+  texts,
+  wordIndex = 0,
+  charIndex = 0,
+  isDeleting = false,
+) {
+  const currentWord = texts[wordIndex];
+  const currentChar = currentWord.substring(0, charIndex);
+  element.textContent = currentChar;
+
+  if (!isDeleting && charIndex < currentWord.length) {
+    setTimeout(() => autoType(element, texts, wordIndex, charIndex + 1), 200);
+  } else if (isDeleting && charIndex > 0) {
+    setTimeout(
+      () => autoType(element, texts, wordIndex, charIndex - 1, isDeleting),
+      100,
+    );
+  } else {
+    if(isDeleting) {
+        setTimeout(() => autoType(element, texts, (wordIndex + 1) % texts.length, charIndex, !isDeleting), 1000);
+    } else {
+        setTimeout(() => autoType(element, texts, wordIndex, charIndex, !isDeleting), 1000);
+    }
+  }
 }
 
 autoType(profile, profileTexts);
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,33 +2,33 @@ const { iconsPlugin, dynamicIconsPlugin } = require("@egoist/tailwindcss-icons")
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./*.{html,js}"],
-    theme: {
-        container: {
-            center: true,
-            padding: "1rem",
-        },
-        extend: {
-            fontFamily: {
-                roboto: ["Roboto", "sans-serif"],
-            },
-            colors: {
-                primary: "#12D393",
-                background: "#070A13",
-            },
-            screens: {
-                "2xl": "1320px",
-            },
-            animation: {
-                type: "type 4s cubic-bezier(0.4, 0, 0.6, 1) infinite",
-            },
-            keyframes: {
-                type: {
-                    "40%, 60%": { left: "100%", width: "0" },
-                    "100%": { left: "0%", width: "100%" },
-                },
-            },
-        },
+  content: ["./*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "1rem",
     },
-    plugins: [iconsPlugin(), dynamicIconsPlugin()],
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
+      colors: {
+        primary: "#12D393",
+        background: "#070A13",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+      keyframes: {
+        blink: {
+          "0%, 100%": { opacity: "1" },
+          "50%": { opacity: "0" },
+        },
+      },
+      animation: {
+        blink: "blink 1.5s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+      },
+    },
+  },
+  plugins: [iconsPlugin(), dynamicIconsPlugin()],
 };
